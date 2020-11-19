fc087304b92992fd61bfeca3d90dc1e64061fa42
Faith Magras <bdanielle092@gmail.com>
2020-11-19 12:02:52 -0600

    working on startsWith L

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Linq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Find the words in the collection that start with the letter 'L'
+            List<string> fruits = new List<string>() { "Lemon", "Apple", "Orange", "Lime", "Watermelon", "Loganberry" };
+
+            Enumerable<string> LFruits = from fruit in fruits.Where(StartsWith == "L");
+            Console.WriteLine(LFruits); 
+
+        }
+     
+    }
+}
